4ce1d7eaa547a17d65c2d5c98fbb2fca03464c7d
Bryce <nash31@korea.ac.kr>
2019-11-13 23:11:21 +0900

    동네예보

--- board/src/main/java/board/controller/BoardController.java
+++ board/src/main/java/board/controller/BoardController.java
@@ -1,17 +1,25 @@
 package board.controller;
 
+import java.io.File;
+import java.net.URLEncoder;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import board.dto.BoardDto;
+import board.dto.BoardFileDto;
 import board.service.BoardService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -64,6 +72,12 @@ public class BoardController {
 	
 	@RequestMapping("/board/updateBoard.do")
 	public String updateBoard(BoardDto board) throws Exception{
+		log.debug(board.getTitle());
+		log.debug(board.getCreatedDatetime());
+		log.debug(board.getCreatorId());
+		log.debug("" + board.getBoardIdx());
+		log.debug(board.toString());
+		
 		boardService.updateBoard(board);
 		return "redirect:/board/openBoardList.do";
 	}
@@ -73,5 +87,22 @@ public class BoardController {
 		boardService.deleteBoard(boardIdx);
 		return "redirect:/board/openBoardList.do";
 	}
-		
+	
+	@RequestMapping("/board/downloadBoardFile.do")
+	public void downloadBoardFile(@RequestParam int idx, @RequestParam int boardIdx, HttpServletResponse response) throws Exception{
+			BoardFileDto boardFile = boardService.selectBoardFileInformation(idx, boardIdx);
+			if(!ObjectUtils.isEmpty(boardFile)) {
+				String fileName = boardFile.getOriginalFileName();
+				
+				byte[] files =FileUtils.readFileToByteArray(new File(boardFile.getStoredFilePath()));
+				
+				response.setContentType("application/octet-stream");
+				response.setHeader("Content-Disposition", "attachment; fileName=\"" + URLEncoder.encode(fileName,"UTF-8") + "\";");
+				response.setHeader("Content-Transfer-Encoding", "binary");
+				
+				response.getOutputStream().write(files);
+				response.getOutputStream().flush();
+				response.getOutputStream().close();
+			}
+	}
 }
